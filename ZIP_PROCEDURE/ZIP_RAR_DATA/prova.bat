7496107d3e346ecb8f9ef0b4976ff568002aaed4
icivixwalter <47453186+icivixwalter@users.noreply.github.com>
2022-11-15 23:43:25 +0100

    AGG

--- ZIP_PROCEDURE/ZIP_RAR_DATA/prova.bat
+++ ZIP_PROCEDURE/ZIP_RAR_DATA/prova.bat
@@ -18,16 +18,24 @@ goto NOTA
 
 
 
+:------------------------------NOME FILE PROFESSATO
+@SET FileRar1= 1_ES_ARCHIVIO_OPZIONE_Ta.RAR 
 
-:------------------------------NOME FILE PROCESSATO DAL 10 MARZO A SETTEMBRE
-@SET FileRar1=1_ES_ARCHIVIO_OPZIONE_TaC_TbC.RAR 
-@SET FileRar2=2_ES_ARCHIVIO_OPZIONE_TaC_TbC.RAR 
+@REM 1 ESEMPIO COME PARAMETRI DALLA PATH CORRENTE
+@REM PROCESSAMENTO FILE con l'opzione -ta = successivi AFTER al 11-10-2022 
+@REM.................da pathCorrente il FILE DA PROCESSARE con LISTA FILE compresa path
+@REM    
 
 
-@REM solo NOVEMBRE e con parametri tacm = After creazione e modifica + tbcm before la data di modifica e di creazione - controlla i vari effetti 
-REM 
-REM c:\CASA\WinRAR\Rar.exe U -tsm1 -tsc1  -log="%FileRar2%elencovolumi.txt" -logf="%FileRar2%_elencofile.txt" -TaM2022:11:1 -TBMC2022:11:30 %FileRar2% @ZIP_RAR_N03_DATA_TAC-TBC_Listafile.txt
-c:\CASA\WinRAR\Rar.exe  a -r -taMC:2022:10:1 -tbCM:2022:10:30 %FileRar1% ZIP_RAR_N03_DATA_TAC-TBC_Listafile.txt
+rem c:\CASA\WinRAR\Rar.exe  	a -r -taMC2022:11:1 -tbM2022:11:30 %FileRar% @ZIP_RAR_N03_DATA_TAC-TBC_Listafile.txt
+rem     c:\CASA\WinRAR\Rar.exe  a -ta2022-8-5                      %FileRar% @Listafile.txt  
+c:\CASA\WinRAR\Rar.exe  a -r -taMC:2022:10:1 -tbCM:2022:10:30 %FileRar1% @ZIP_RAR_N03_DATA_TAC-TBC_Listafile.txt
+
+
+
+
+
+
          
 @REM Ritardo per 7 secondi
 @choice /C:X /N /T:3 /D:X > NUL		

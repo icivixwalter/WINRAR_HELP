fe03f731381b81f49bb439ac30339e8f2e55adbc
walter <47453186+icivixwalter@users.noreply.github.com>
2022-11-29 14:16:14 +0100

    AGG

--- ZIP_PROCEDURE/ZIP_RAR_DATA/prova.bat
+++ ZIP_PROCEDURE/ZIP_RAR_DATA/prova.bat
@@ -8,33 +8,24 @@ goto NOTA
 
 
 
-@ESEMPIO LISTA FILE.TXT CON PATH COMPRESE
-:------------------------------dove si trova il file lista.txt nella path corrente
-@SET PathOrigine_s=y:\LAVORI_PUBBLICI_FINO_AL_30_09_2019\Valter\OPERE_PUBBLICHE\ZZ99_SALVATAGGI_BACKUP\SALVATAGGI\70_ATTI_GENERICI\BAT_RAR_LISTA_FILE\
-:------------------------------dove si trovano i file
-@SET PathOrigineII_s=y:\LAVORI_PUBBLICI_FINO_AL_30_09_2019\Valter\OPERE_PUBBLICHE\70_ATTI_GENERICI\
 
 
 
 
+NOTE_BAT_s
+"@ESEMPIO LISTA FILE.TXT CON PATH COMPRESE
+:------------------------------dove si trova il file lista.txt nella path corrente"
+:-----------------COMM= dove si trova il file lista.txt nella path corrente
+PathOrigine_s=y:\LAVORI_PUBBLICI_FINO_AL_30_09_2019\Valter\OPERE_PUBBLICHE\ZZ99_SALVATAGGI_BACKUP\SALVATAGGI\70_ATTI_GENERICI\BAT_RAR_LISTA_FILE\
+:------------------------------dove si trovano i file, PATH DI ORIGINE
+PathOrigineII_s=y:\LAVORI_PUBBLICI_FINO_AL_30_09_2019\Valter\OPERE_PUBBLICHE\70_ATTI_GENERICI\
+NOME FILE PROCESSATO
+FileRar=1_ES_ARCHIVIO_OPZIONE_Ta.RAR
+1 ESEMPIO COME PARAMETRI DALLA PATH CORRENTE
+PROCESSAMENTO FILE con l'opzione -ta = successivi AFTER al 11-10-2022
+.................da pathCorrente il FILE DA PROCESSARE con LISTA FILE compresa path
 
-:------------------------------NOME FILE PROFESSATO
-@SET FileRar1= 1_ES_ARCHIVIO_OPZIONE_Ta.RAR 
-
-@REM 1 ESEMPIO COME PARAMETRI DALLA PATH CORRENTE
-@REM PROCESSAMENTO FILE con l'opzione -ta = successivi AFTER al 11-10-2022 
-@REM.................da pathCorrente il FILE DA PROCESSARE con LISTA FILE compresa path
-@REM    
-
-
-rem c:\CASA\WinRAR\Rar.exe  	a -r -taMC2022:11:1 -tbM2022:11:30 %FileRar% @ZIP_RAR_N03_DATA_TAC-TBC_Listafile.txt
-rem     c:\CASA\WinRAR\Rar.exe  a -ta2022-8-5                      %FileRar% @Listafile.txt  
 c:\CASA\WinRAR\Rar.exe  a -r -taMC:2022:10:1 -tbCM:2022:10:30 %FileRar1% @ZIP_RAR_N03_DATA_TAC-TBC_Listafile.txt
-
-
-
-
-
 
          
 @REM Ritardo per 7 secondi
